a07b565a5a6323b284a6b13b6d35c90b73b446dc
Steve Roy <scr44@case.edu>
2019-10-09 19:47:56 -0400

    Added classes and eat response tests.

---
+++ 19_Capstone/Capstone/Models/Item.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+    public class Item
+    {
+        public Item(string name, string snackType, decimal price, string slot, int count)
+        {
+            Name = name;
+            SnackType = snackType;
+            Price = price;
+            Slot = slot;
+            Count = count;
+        }
+
+        public string Name { get; }
+        public string SnackType { get; }
+        public decimal Price { get; }
+        public string Slot { get; }
+        public string EatResponse
+        {
+            get
+            {
+                switch(SnackType)
+                {
+                    case "chips":
+                        return "Crunch Crunch, Yum!";
+                    case "candy":
+                        return "Munch Munch, Yum!";
+                    case "drink":
+                        return "Glug Glug, Yum!";
+                    case "gum":
+                        return "Chew Chew, Yum!";
+                    default:
+                        throw new ArgumentException("Cannot get eat response from an invalid snack type.");
+                }
+
+            }
+        }
+        public int Count { get; set; }
+    }
+}
---
+++ 19_Capstone/CapstoneTests/ItemTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Capstone.Models;
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class ItemTests
+    {
+        Item reeses = new Item("Reeses", "candy", 3.20M, "A1", 5);
+        Item lays = new Item("Lays", "chips", 4.20M, "A2", 4);
+        Item cherryCoke = new Item("Cherry Coke", "drink", 7.00m, "C6", 5);
+        Item bigChew = new Item("Big League Chew", "gum", 3.40m, "D2", 0);
+
+        [TestMethod]
+        public void EatResponseTests()
+        {
+            Assert.AreEqual("Munch Munch, Yum!", reeses.EatResponse);
+            Assert.AreEqual("Crunch Crunch, Yum!", lays.EatResponse);
+            Assert.AreEqual("Glug Glug, Yum!", cherryCoke.EatResponse);
+            Assert.AreEqual("Chew Chew, Yum!", bigChew.EatResponse);
+        }
+    }
+}
