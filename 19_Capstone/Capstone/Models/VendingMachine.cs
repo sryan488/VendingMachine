5cb6f23579861e388157274ab823dc9cbc2742ec
Kit Partezana <kit.partezana@gmail.com>
2019-10-10 10:24:52 -0400

    Vending Machine Class changes

--- 19_Capstone/Capstone/Models/VendingMachine.cs
+++ 19_Capstone/Capstone/Models/VendingMachine.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace Capstone.Models
 {
+    /// <summary>
+    /// Constructor
+    /// Get Inventory from inventory file 'vendingmachine.csv'
+    /// </summary>
     public class VendingMachine
     {
-
+        public VendingMachine(string inventoryFile)
+        {
+            using (StreamReader sr = new StreamReader(inventoryFile))
+            {
+                while (!sr.EndOfStream)
+                {
+                    try
+                    {
+                        string inventoryLine = sr.ReadLine();
+                        string[] itemFields;
+                        itemFields = inventoryLine.Split("|");
+                        Item inputItem = new Item(itemFields[1], itemFields[3], decimal.Parse(itemFields[2]), itemFields[0], 5);
+                        Inventory.AddItem(inputItem);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("InventoryFileIssue");
+                    }
+                }
+            }
+            
+        }
     }
 }
